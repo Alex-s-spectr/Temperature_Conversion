8ad756f461a366a473c170e73c3f169bc22abd03
Alex <2291187@cegepmontpetit.ca>
2024-08-08 21:06:33 -0400

    first commit

---
+++ Temperature_Conversion/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Degré Celsius : "); 
+ 
+int Celsius = int.Parse(Console.ReadLine()) ;
+
+Console.WriteLine($"Degré Fahrenheit : {(Celsius * 9/5)+32 }"+"°F");
